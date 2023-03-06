ba95a7a7f0208aeeefb2ab5003027e69b107b3fc
Diganta Roy <roydiganta_34@yahoo.com>
2023-03-06 18:30:57 -0500

    Poller Working + Models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class ManufacturerVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100, unique=True)
+
+    def get_api_url(self):
+        return reverse("api_manufacturer", kwargs={"pk": self.id})
+class VehicleModelVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100)
+    picture_url = models.URLField()
+
+    manufacturer = models.ForeignKey(
+        ManufacturerVO,
+        related_name="models",
+        on_delete=models.CASCADE,
+    )
+    def get_api_url(self):
+        return reverse("api_vehicle_model", kwargs={"pk": self.id})
+        
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    color = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    vin = models.CharField(max_length=17, unique=True)
+
+    model = models.ForeignKey(
+        VehicleModelVO,
+        related_name="automobiles",
+        on_delete=models.CASCADE,
+    )
+    def get_api_url(self):
+        return reverse("api_automobile", kwargs={"vin": self.vin})
+
+    
--- service/poll/poller.py
+++ service/poll/poller.py
@@ -10,14 +10,24 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
 # Import models from service_rest, here.
+from service_rest.models import AutomobileVO
 # from service_rest.models import Something
 
 def poll():
     while True:
         print('Service poller polling for data')
         try:
-            # Write your polling logic, here
-            pass
+            response = requests.get("http://inventory-api:8000/api/automobiles/")
+            content = json.loads(response.content)
+            for automobile in content["automobiles"]:
+                print(automobile)
+                AutomobileVO.objects.update_or_create(
+                    import_href=automobile["href"],
+                    color=automobile['color'],
+                    year=automobile['year'],
+                    vin=automobile['vin'],
+                    model=automobile['model'],
+                )
         except Exception as e:
             print(e, file=sys.stderr)
         time.sleep(60)
